a6d27dd3c5f12a848310d9be05964790220066a6
Chris Boveda <cboveda@asu.edu>
2022-04-30 22:16:36 -0700

    Create About, Projects, Skills, and Contact sections

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Navbar from "./components/Navbar"
+import Projects from "./components/Projects"
+import Skills from "./components/Skills"
+import Testimonials from "./components/Testimonials"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="text-stone-100 bg-slate-700 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,45 @@
+export const projects = [
+    {
+        title: "Far Flung: A Psyche Mission Adventure",
+        subtitle: "Unity and C#",
+        description:
+            "Web-based educational game that features five unique mini-games and an engaging narrative campaign.",
+        image: "./project-1.gif",
+        link: "/",
+    },
+    {
+        title: "Far Flung: A Psyche Mission Adventure",
+        subtitle: "Unity and C#",
+        description:
+            "Web-based educational game that features five unique mini-games and an engaging narrative campaign.",
+        image: "./project-2.gif",
+        link: "/",
+    },
+    {
+        title: "Far Flung: A Psyche Mission Adventure",
+        subtitle: "Unity and C#",
+        description:
+            "Web-based educational game that features five unique mini-games and an engaging narrative campaign.",
+        image: "./project-3.gif",
+        link: "/",
+    },
+    {
+        title: "Far Flung: A Psyche Mission Adventure",
+        subtitle: "Unity and C#",
+        description:
+            "Web-based educational game that features five unique mini-games and an engaging narrative campaign.",
+        image: "./project-4.gif",
+        link: "/",
+    },
+];
+
+export const skills = [
+    "JavaScript",
+    "Java",
+    "C",
+    "C#",
+    "Agile/Scrum",
+    "Project Management",
+    "Team Leadership",
+    "Mentoring",
+]
