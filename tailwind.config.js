6cd3776c394cfbe81c0f14baba4681f8aed767cc
Chris Boveda <cboveda@asu.edu>
2022-05-08 12:15:36 -0700

    Add progress bar for proficiency level

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+const percentageWidth = require('tailwindcss-percentage-width')
 module.exports = {
+  mode: 'jit',
   content: ["./src/**/*.{html,js}"],
+  purge: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
   theme: {
     extend: {
     },
   },
-  plugins: [],
+  plugins: [
+    percentageWidth,
+  ],
 }
