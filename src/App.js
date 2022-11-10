24811fb2919a7bd37c65893c9ff2d9d78c5a721d
Chris Boveda <cboveda@asu.edu>
2022-11-09 21:50:53 -0800

    Fix unused vars

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import About from "./components/About"
 import Contact from "./components/Contact"
 import Navbar from "./components/Navbar"
-import Projects from "./components/Projects"
-import Skills from "./components/Skills"
+// import Projects from "./components/Projects"
+// import Skills from "./components/Skills"
 import Endorsements from "./components/Endorsements"
 
 function App() {
